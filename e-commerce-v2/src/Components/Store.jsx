bf00114f7720e0aa9bf414b43ea908e70fdb9024
jemima139 <165708488+jemima139@users.noreply.github.com>
2024-07-23 15:21:19 -0400

    Made changes to code

--- e-commerce-v2/src/Components/Store.jsx
+++ e-commerce-v2/src/Components/Store.jsx
@@ -12,7 +12,7 @@ const ProductList = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get("http://localhost:3004/api/products");
+        const response = await axios.get("/api/products");
         setProducts(response.data);
         setLoading(false);
       } catch (error) {
